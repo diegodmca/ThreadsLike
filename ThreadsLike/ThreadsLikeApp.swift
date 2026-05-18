317e22e142d53da8041f3d984cf62360d52e9c2b
Diego Carvalho <diegodmca@outlook.com>
2026-05-18 16:27:47 -0300

    Initial Commit

---
+++ ThreadsLike/ThreadsLikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsLikeApp.swift
+//  ThreadsLike
+//
+//  Created by Diego Carvalho on 18/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsLikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
