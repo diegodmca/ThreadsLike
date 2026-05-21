044d156fc2d21680b7b94701f95bd7c5ff401592
Diego Carvalho <diegodmca@outlook.com>
2026-05-21 00:04:16 -0300

    Criando tela de login

--- ThreadsLike/Core/Authentication/Views/LoginView.swift
+++ ThreadsLike/Core/Authentication/Views/LoginView.swift
@@ -8,10 +8,80 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        NavigationStack{
+            VStack{
+                Spacer()
+                
+                Image("threadslike-app-icon")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 120, height: 120)
+                    .padding()
+                VStack{
+                    TextField("Enter Your e-mail", text: $email)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                    
+                    SecureField("Enter Your Password", text:  $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 24)
+                }
+                
+                NavigationLink {
+                    Text("Forgot Password")
+                } label: { Text("Forgot Password?")
+                                .font(.footnote)
+                                .fontWeight(.semibold)
+                                .padding(.vertical)
+                                .padding(.trailing, 28)
+                                .foregroundColor(.black)
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                Button{
+                    
+                } label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 352, height: 44)
+                        .background(.black)
+                        .cornerRadius(8)
+                        
+                }
+                Spacer()
+                
+                Divider()
+                
+                NavigationLink{
+                    Text("Registration View")
+                } label: {
+                    HStack(spacing: 3){
+                        Text("Don't have an account?")
+                        
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .foregroundColor(.black)
+                    .font(.footnote)
+                    
+                }
+                .padding(.vertical, 16)
+                
+            }
+                    
+                }
+            }
+        }
 
 #Preview {
     LoginView()
