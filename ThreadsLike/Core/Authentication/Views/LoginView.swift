bad5bed7c108adcdbe87148baaff20ae6ac54dc6
Diego Carvalho <diegodmca@outlook.com>
2026-05-20 23:55:45 -0300

    first commit

---
+++ ThreadsLike/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  ThreadsLike
+//
+//  Created by Diego Carvalho on 20/05/26.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
