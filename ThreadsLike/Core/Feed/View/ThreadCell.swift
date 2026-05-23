39fe43664de91c17d95427b8cc24b28061e47899
Diego Carvalho <diegodmca@outlook.com>
2026-05-22 22:42:32 -0300

    Feed View UI

---
+++ ThreadsLike/Core/Feed/View/ThreadCell.swift
@@ -0,0 +1,84 @@
+//
+//  ThreadCell.swift
+//  ThreadsLike
+//
+//  Created by Diego Carvalho on 22/05/26.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        VStack{
+            HStack(alignment: .top, spacing: 12){
+                Image("the-hunter")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading, spacing: 4){
+                    HStack {
+                        Text("TheHunter1")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                        Spacer()
+                        
+                        Text("10m")
+                            .font(.caption)
+                            .foregroundColor(Color(.systemGray3))
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "ellipsis")
+                                .foregroundColor(Color(.darkGray))
+                        }
+                        
+                    }
+                    
+                    Text("From Bloodborne")
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                    
+                    HStack(spacing: 16){
+                        Button {
+                            
+                        } label:{
+                            Image(systemName: "heart")
+                        }
+                        
+                        Button {
+                            
+                        } label:{
+                            Image(systemName: "bubble.right")
+                        }
+                        
+                        Button {
+                            
+                        } label:{
+                            Image(systemName: "arrow.rectanglepath")
+                        }
+                        
+                        Button {
+                            
+                        } label:{
+                            Image(systemName: "paperplane")
+                        }
+                        
+                    }
+                    .foregroundColor(.black)
+                    .padding(.vertical, 8)
+                    
+                }
+                    
+            }
+            Divider()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ThreadCell()
+}
